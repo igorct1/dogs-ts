7ceaaf18ca6f795622db42dce5ef39a75e6f185a
igoreason <igorct1@gmail.com>
2023-06-20 14:40:46 -0300

    feat: header component

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,11 +1,17 @@
+import { Dogs } from '../icons/dogs';
 import styles from './header.module.css';
 import { Link } from 'react-router-dom';
+
 export function Header() {
 	return (
 		<header className={styles.header}>
-			<nav className="container">
-				<Link to="/">Home</Link>
-				<Link to="/login">Login / Criar</Link>
+			<nav className={`${styles.nav} container`}>
+				<Link to="/" aria-label="Dogs - Home" className={styles.logo}>
+					<Dogs />
+				</Link>
+				<Link to="/login" className={styles.login}>
+					Login / Criar
+				</Link>
 			</nav>
 		</header>
 	);
