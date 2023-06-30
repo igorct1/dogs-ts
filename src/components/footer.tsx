4ff3f087c7bece54e055b111b8f0cbbee100de57
igoreason <igorct1@gmail.com>
2023-06-30 18:19:24 -0300

    feat: footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,5 +1,11 @@
+import { DogsFooter } from '../icons/dogs-footer';
 import styles from './footer.module.css';
 
 export function Footer() {
-	return <footer className={styles.footer}></footer>;
+	return (
+		<footer className={styles.footer}>
+			<DogsFooter />
+			<p>Dogs. Alguns direitos reservados.</p>
+		</footer>
+	);
 }
